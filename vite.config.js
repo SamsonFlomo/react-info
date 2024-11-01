d07c6d612d26b861466c476ad2ad799494e447a7
SamsonFlomo <flomosamson125@gmail.com>
2024-11-01 14:46:26 +0000

    neglify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/react-info/"
-})
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [react()],
+    base: '',
+  };
+
+  if (mode === 'development' || mode === 'github') {
+    config.base = '/react-info/';
+  }
+
+  return config;
+});
